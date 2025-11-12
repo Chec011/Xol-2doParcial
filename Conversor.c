a7184637186f4a5f972b5fac1511d10db4cd4ccf
Chec011 <melx101120hdfnpla4@lake.edu.mx>
2025-11-11 21:41:56 -0600

    Update Conversor.c

--- Conversor.c
+++ Conversor.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+//Convierte metros a millas//
 main(){
     float Valor;
     float Resultado;
 
     printf("Ingresa el valor a convertir \n");
     scanf("%f", &Resultado);
-    Resultado= Valor/1.609;
+    Resultado= Valor/1609;
     printf("Tu resultado es de: %f", Resultado);
 }
+
